964cf1e4a8b842dcc6b60d4a88821619f9a5b80a
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2023-08-25 21:41:55 +0530

    Add files via upload

---
+++ Prime no. identifier.py
@@ -0,0 +1,12 @@
+# A default function for Prime checking conditions
+a = int(input("Enter an input number:"))
+if a > 1:  
+        for j in range(2, a):  
+            if (a % j) == 0:  
+                print(a, "is not a prime number")  
+                break  
+        else:  
+            print(a, "is a prime number")
+else:
+        print(a, "is not a prime number")  
+  
