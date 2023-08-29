c3781936be54ca2afe41c20b4fe1de5b3afd3272
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2023-08-30 00:05:20 +0530

    Add files via upload

---
+++ Table (while).py
@@ -0,0 +1,10 @@
+num = int(input( "Enter the number : "))    
+i = 1
+                
+# using for loop to perform multiplication 10 times    
+print("Multiplication Table of : ")
+                
+while i<=10:  
+    num = num * 1  
+    print(num,'x',i,'=',num*i)  
+    i += 1  
