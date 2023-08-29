0c0bd018ded6e2b31fdce39cff6534bd90bd56ef
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2023-08-30 00:06:10 +0530

    Add files via upload

---
+++ Time cal.py
@@ -0,0 +1,6 @@
+#Q.5
+
+time = int(input("enter an hour between 1 to 12 = "))
+headhour = int(input("enter the a head hour = "))
+sum = time + headhour
+print("Time at that time would be:",(sum<= 12 and sum)or sum-12)
