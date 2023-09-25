0ae9e9636f7ad9ee4c3bac7402272c3b5a9904fe
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2023-09-25 23:50:54 +0530

    Update Password generator.py

--- Password generator.py
+++ Password generator.py
@@ -5,4 +5,8 @@ def generate_password(length):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
+    
+length = int(input("Enter the length of the password: "))
+password = generate_password(length)
+print("Generated password:", password)
 
