452cbe6fa26d14f697666dad7bad529987ca93e7
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2023-09-25 23:49:49 +0530

    Add files via upload

---
+++ Password generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
