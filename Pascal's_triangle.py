15b78a02982e8ba0cec5a91419a239d0c909053b
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-01-01 22:02:48 +0530

    Add files via upload

---
+++ Pascal's_triangle.py
@@ -0,0 +1,12 @@
+n = int(input("No of rows:"))
+triangle = []
+
+for i in range(n):
+    row = [1] * (i + 1)
+    if i > 1:
+        for j in range(1, i):
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    triangle.append(row)
+
+for row in triangle:
+    print(row)
