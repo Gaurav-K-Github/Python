b5b3d38e4c5d680528b1aa8788b4314b26a3fd02
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-01-08 22:01:34 +0530

    Update Center star pattern.py

--- Center star pattern.py
+++ Center star pattern.py
@@ -1,20 +1,12 @@
-# Without using a function
-
-# number of rows
 n = 5
 
-# number of spaces
 k = n - 1
 
-# outer loop to handle number of rows
 for i in range(0, n):
  
-    # inner loop to handle number spaces
-    # values changing acc. to requirement
     for j in range(0, k):
         print(end=" ")
  
-    # decrementing k after each loop
     k = k - 1
  
     # inner loop to handle number of columns
