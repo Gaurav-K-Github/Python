d46046cd04e2f2238d01b3e17b19249e90da7dc7
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-01-17 23:57:30 +0530

    Update Guess a number.py

--- Games/Guess a number.py
+++ Games/Guess a number.py
@@ -1,6 +1,6 @@
 import random
 
-def number_guessing_game():
+def guess():
     lower_bound = int(input("Enter the lower bound of the range: "))
     upper_bound = int(input("Enter the upper bound of the range: "))
     target_number = random.randint(lower_bound, upper_bound)
