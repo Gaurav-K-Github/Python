d46a819d46e3179d1ee622aa1c348ea8f49329bb
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-01-17 23:56:41 +0530

    Add files via upload

---
+++ Games/Guess a number.py
@@ -0,0 +1,19 @@
+import random
+
+def number_guessing_game():
+    lower_bound = int(input("Enter the lower bound of the range: "))
+    upper_bound = int(input("Enter the upper bound of the range: "))
+    target_number = random.randint(lower_bound, upper_bound)
+    attempts = 0
+    while True:
+        attempts += 1
+        guess = int(input("Enter your guess: "))
+        if guess == target_number:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+        elif guess < target_number:
+            print("Your guess is too low. Try again.")
+        else:
+            print("Your guess is too high. Try again.")
+
+number_guessing_game()
