b550c17160e56730fadaf39a1dba108449645544
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-01-27 23:39:18 +0530

    Update Story generator.py

--- Games/Story generator.py
+++ Games/Story generator.py
@@ -4,11 +4,11 @@ import random
 #Lists of phrases
 sentence_starter = ['About 100 years ago', 'In the 20 BC', 'Once upon a time']
 character = ['there lived a king.', 'there was a man named Jack.', 'there lived a farmer.']
-time = ['One day', 'One full-moon night']
+time = ['One day', 'One full-moon night','busiest part of the day']
 story_plot = ['he was passing by', 'he was going for a picnic to']
 place = ['the mountains', 'the garden']
 second_character = ['he saw a man', 'he saw a young lady']
 age = ['who seemed to be in late 20s', 'who seemed very old and feeble']
-work = ['searching something.', 'digging a well on roadside.']
+work = ['searching something.', 'digging a well on roadside.','waiting for someone.']
 
 print(random.choice(sentence_starter) + ' ' + random.choice(character) + ' ' + random.choice(time) + ', ' + random.choice(story_plot) + ' ' + random.choice(place) + ', ' + random.choice(second_character) + ' ' + random.choice(age) + ' ' + random.choice(work))
