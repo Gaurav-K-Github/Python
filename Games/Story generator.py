322750f6467c2f48c1f0ba4d54992cc0f0773853
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-01-27 23:37:15 +0530

    Update Story generator.py

--- Games/Story generator.py
+++ Games/Story generator.py
@@ -1,5 +1,7 @@
 #Python Story Generator
 import random
+
+#Lists of phrases
 sentence_starter = ['About 100 years ago', 'In the 20 BC', 'Once upon a time']
 character = ['there lived a king.', 'there was a man named Jack.', 'there lived a farmer.']
 time = ['One day', 'One full-moon night']
