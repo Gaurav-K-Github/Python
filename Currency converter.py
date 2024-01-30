dc6907cca3b543c39be2d7eb566a7d3e4c194891
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-01-30 23:54:29 +0530

    Update Currency converter.py

--- Currency converter.py
+++ Currency converter.py
@@ -1,7 +1,6 @@
-from currency_converter import CurrencyConverter
-c = CurrencyConverter()
+c = int(input("Conversion rate:"))
 amount = 100
 from_currency = 'USD'
-to_currency = 'EUR'
-converted_amount = c.convert(amount, from_currency, to_currency)
+to_currency = 'INR'
+converted_amount = (amount, from_currency, to_currency)
 print(f"{amount} {from_currency} = {converted_amount} {to_currency}")
