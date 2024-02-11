8bd4445730f05e2ed7c2e0c6ece289a7960ad36b
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-02-11 20:07:17 +0530

    Update Die per wafer calculator with GUI.py

--- GUI/Die per wafer calculator with GUI.py
+++ GUI/Die per wafer calculator with GUI.py
@@ -1,13 +1,13 @@
-#Die per wafer calculator
 import math
-c_input=input("Choose conversion (mm/inch) for input:")
 
-if c_input=='mm':
+c_input = input("Choose conversion (mm/inch) for input: ")
+
+if c_input == 'mm':
     print()
-    d=float(input("Enter the diuameter of the wafer:"))
-    s=float(input("Enter the die size:"))
-    print(d*math.pi((d/4*s)-(1/math.sqrt(2*s))))
-elif c_input=='inch':
-    d=float(input("Enter the diuameter of the wafer:"))*25.4
-    s=float(input("Enter the die size:"))*25.4
-    print(d*math.pi((d/4*s)-(1/math.sqrt(2*s))))
+    d = float(input("Enter the diameter of the wafer: "))
+    s = float(input("Enter the die size: "))
+    print(d * math.pi * ((d / 4 * s) - (1 / math.sqrt(2 * s))))
+elif c_input == 'inch':
+    d = float(input("Enter the diameter of the wafer: ")) * 25.4
+    s = float(input("Enter the die size: ")) * 25.4
+    print(d * math.pi * ((d / 4 * s) - (1 / math.sqrt(2 * s))))
