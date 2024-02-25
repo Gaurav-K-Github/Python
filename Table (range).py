5d33500dfdf402c0aa753d7f2417e277a161568f
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-02-25 23:53:29 +0530

    Delete Table (range).py

--- Table (range).py
+++
@@ -1,7 +0,0 @@
-num = int(input(" Enter the number : "))
-
-# using for loop to iterate multiplication 10 times     
-print("Multiplication Table of : ")
-
-for i in range(1,11):    
-   print(num,'x',i,'=',num*i)  
