4d47a2ca240a8d14943b97a19f2cfc70412a7524
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-02-28 23:23:37 +0530

    Create Palindrome.py

---
+++ Concepts/Palindrome.py
@@ -0,0 +1,14 @@
+l1=[]
+l2=[]
+a=int(input('Enter a no.:'))
+l=len[a]
+r=l%2
+if r==0:
+    l1=[:(l/2)]
+elif r!=0:
+    l2=[:((l/2)*(-1))]
+    
+if l1==l2:
+    print('yes')
+elif l1!=l2:
+    print('no')    
