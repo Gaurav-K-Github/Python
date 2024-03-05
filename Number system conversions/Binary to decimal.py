2b934b8054eb795265a7f526c30493065a054d40
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-03-05 22:49:15 +0530

    Update Binary to decimal.py

--- Number system conversions/Binary to decimal.py
+++ Number system conversions/Binary to decimal.py
@@ -5,7 +5,7 @@ power=len(binary) - 1
 
 for digit in binary:
     if digit=='1':
-        decimal_number+=2**power
+        decimal+=2**power
     power-=1
 
-print("Decimal equivalent:", decimal_number)
+print("Decimal equivalent:", decimal)
