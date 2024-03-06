06798b06201643a5ca41138ba74aa6f438e9141d
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-03-06 22:26:38 +0530

    Update Online Voting System.py

--- Projects/Online Voting System.py
+++ Projects/Online Voting System.py
@@ -3,3 +3,9 @@ class VotingSystem:
         self.users = {}
         self.candidates = {}
         self.votes = {}
+
+    def register_user(self, username, password):
+        if username in self.users:
+            print("User already exists. Please choose another username.")
+            return False
+        self.users[username] = password
