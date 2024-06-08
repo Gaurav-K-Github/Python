7451fa09d6de84442ae6d687c38c7d1955b99f02
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2024-06-09 00:37:09 +0530

    Create Main_window.py

---
+++ Tkinter/Main_window.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+root = tk.Tk() # Create the main window
+root.title("My First Tkinter Window")
+root.geometry("400x300") # Set the window size
+root.mainloop() # Run the application
